a985512ab5c0d6e2091534f8263996394a6cf85a
Cameron Byroad <cameronbyroad@gmail.com>
2022-12-26 15:42:33 -0600

    Implemented filtering comments for the single post in blog/views.py and blogpost.html

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 # Create your views here.
 from .forms import BlogPostForm
-from .models import BlogPost
+from .models import BlogPost, Comments
 
 def createPost(request):
     form = BlogPostForm()        
@@ -20,25 +20,25 @@ def createPost(request):
     return render(request, 'create_post.html', context)
 
 def viewPost(request, pk):
-    post = BlogPost.objects.filter(id=pk)
+    post = BlogPost.objects.get(id=pk)
     author = BlogPost.objects.values_list('author__username', flat=True).get(id=pk)
+    # need to query for comments that only apply for the single blogpost
+    comments = post.comments_set.all()
 
     
-    context = {'post':post, 'author':author}
+    context = {'post':post, 'author':author, 'comments':comments}
     return render(request, 'blogpost.html', context)
 
 def deletePost(request, pk):
     post = BlogPost.objects.get(id=pk)
     author = BlogPost.objects.values_list('author__username', flat=True).get(id=pk)
     
-    if author == request.user.username:
-        if request.method == 'POST':
-            post.delete()
-            return redirect('home')
+    if request.method == 'POST':
+        if author == request.user.username:
+                post.delete()
+                return redirect('home')
         else:
-            messages.error(request, 'There was an error deleting this post')
-    else:
-        messages.error(request, 'You are not authorized to do this')
+            messages.error(request, 'You are not authorized to do this')
 
     context = {'post':post, 'author':author}
     return render(request, 'delete_post.html', context)
