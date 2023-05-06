e0db09cba69bff2ecf79ed5afe70b37d56b3d7d7
Cameron Byroad <cameronbyroad@gmail.com>
2023-05-06 17:21:20 -0500

    hide keys

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-(n1rf_6i097*+2e$e)7deh59ohu=*&6!_ot=e56uudli%$qn%d'
 SECRET_KEY=env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
